eb669a53a312f53bc9db71d8e30e8acbf94e615b
sandeepkonakanchi <sxk80270@ucmo.edu>
2018-01-01 15:03:27 -0500

    ui files

---
+++ resources/views/partials/jumbotron.blade.php
@@ -0,0 +1,26 @@
+ <!-- Jumbotron -->
+  <div class="jumbotron test">
+
+    <h1>Welcome to Lara-World</h1>
+
+    <p class="lead">This blog is all about Laravel stuff. Who are Epistemophile about Laravel. Welcome for you guys! I'm just Sharing my skills here. Thanks all. Work Hard n Learn more.  </p>
+
+    <p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p>
+
+    <hr>
+
+  </div>
+
+  <style type="text/css" media="screen">
+
+  .jumbotron {
+  text-align: center;
+  background-color: transparent;
+  padding-top: 2cm;
+}
+.jumbotron .btn {
+  padding: 14px 24px;
+  font-size: 21px;
+}
+  	
+  </style>
