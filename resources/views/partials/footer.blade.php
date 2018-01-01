fbb4cc664219d3557eefcae8e1002f218f3aba52
sandeepkonakanchi <sxk80270@ucmo.edu>
2018-01-01 14:01:43 -0500

    updated ui things

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Myblog-2017   all rights are reserved.</p>
+      </div>
+      <!-- /.container -->
+    </footer>
